65d7b818ad0853b8b371859b8d4d41aa789f833b
Andrewww3006 <andrew_3006@mail.ru>
2023-11-12 22:49:06 +0300

    Different collections  commit

--- src/com/company/vehicles/Main.java
+++ src/com/company/vehicles/Main.java
@@ -1,6 +1,6 @@
 package com.company.vehicles;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class Main {
 
@@ -8,9 +8,9 @@ public class Main {
 	// write your code here
 
         ArrayList<Student> studentList = new ArrayList<>();
-        Student ivanov = new Student("Ivanov", 52294, 2,5,4,3);
-        Student petrov = new Student("Petrov", 52295, 2,1,2,2);
-        Student sidorov = new Student("Sidorov", 52293, 2,1,2,3);
+        Student ivanov = new Student("Ivanov", 52294, 2,10,4,3);
+        Student petrov = new Student("Petrov", 52295, 2,2,2,2);
+        Student sidorov = new Student("Sidorov", 52293, 2,5,2,3);
         studentList.add(ivanov);
         studentList.add(petrov);
         studentList.add(sidorov);
@@ -18,12 +18,35 @@ public class Main {
         admin.displayStudentList();
         //admin.deleteStudent();
         System.out.println();
-        admin.displayStudentList();
+        //admin.displayStudentList();
         //admin.moveToNextCourse();
         //admin.displayStudentList();
-        admin.printStudents(studentList,2);
+        //admin.printStudents(studentList,2);
         Student test = new Student();
-        test.printStudents(studentList,2);
+        //test.printStudents(studentList,2);
 
+        List<Student> linkedListStudents = new LinkedList<>();
+
+        linkedListStudents.addAll(studentList);
+        linkedListStudents.add(2, admin);
+        for(Student student:linkedListStudents)
+            System.out.println(student.getName()+"l");
+
+        Comparator<Student> idComparator = new Comparator <Student>(){
+
+            @Override
+            public int compare(Student c1, Student c2) {
+                return (c1.getMathMark() - c2.getMathMark());
+            }
+        };
+        Queue<Student> priorityQueue = new PriorityQueue<>(idComparator);
+        priorityQueue.add(sidorov);
+        priorityQueue.add(ivanov);
+        priorityQueue.add(petrov);
+
+        System.out.println(priorityQueue.poll().getName());
+        System.out.println(priorityQueue.poll().getName());
+
+        System.out.println( priorityQueue.peek().getName());
     }
 }
